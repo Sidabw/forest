f61edd3538c8b3af27df98c965267a284b756dcd
feiyi <feiyi@zenki.ai>
2019-05-22 19:57:36 +0800

    添加图形验证码demo

--- src/main/java/com/beta/widgets/gp/controller/GraduationProjectController.java
+++ src/main/java/com/beta/widgets/gp/controller/GraduationProjectController.java
@@ -10,8 +10,16 @@
  */
 package com.beta.widgets.gp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * 〈一句话功能简述〉:
@@ -25,6 +33,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/gp")
 public class GraduationProjectController {
+    private static final Logger logger = LoggerFactory.getLogger(GraduationProjectController.class);
+    /**
+     * 生成验证码
+     */
+    @RequestMapping(value = "/getImgVerifyCode")
+    public void getVerify(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.setContentType("image/jpeg");//设置相应类型,告诉浏览器输出的内容为图片
+            response.setHeader("Pragma", "No-cache");//设置响应头信息，告诉浏览器不要缓存此内容
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expire", 0);
+            RandomValidateCodeUtil randomValidateCode = new RandomValidateCodeUtil();
+            randomValidateCode.getRandcode(request, response);//输出验证码图片方法
+        } catch (Exception e) {
+            logger.error("获取验证码失败>>>>   ", e);
+        }
+    }
+
+    /**
+     * 校验验证码
+     */
+    @RequestMapping(value = "/checkVerify", method = RequestMethod.POST, headers = "Accept=application/json")
+    public boolean checkVerify(@RequestParam String verifyInput, HttpSession session) {
+        try{
+            //从session中获取随机数
+            String inputStr = verifyInput;
+            String random = (String) session.getAttribute("RANDOMVALIDATECODEKEY");
+            if (random == null) {
+                return false;
+            }
+            if (random.equals(inputStr)) {
+                return true;
+            } else {
+                return false;
+            }
+        }catch (Exception e){
+            logger.error("验证码校验失败", e);
+            return false;
+        }
+    }
 
 
 }
