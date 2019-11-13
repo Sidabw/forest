ce09b2ebfa036b210dbd95ceab4c7f7a026efa2b
feiyi <feiyi@zenki.ai>
2019-11-13 16:10:29 +0800

    test

--- src/test/java/com/Java/basic/extendstest/Demo.java
+++ src/test/java/com/Java/basic/extendstest/Demo.java
@@ -10,6 +10,11 @@
  */
 package com.Java.basic.extendstest;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 〈一句话功能简述〉:
  * 〈〉
@@ -21,9 +26,24 @@ package com.Java.basic.extendstest;
 public class Demo {
 
     public static void main(String[] args){
-        char a = 'a';
-        int ai = 0;
-//        ai = a + 1;
-        System.out.println(a);
+//        char a = 'a';
+//        int ai = 0;
+////        ai = a + 1;
+//        System.out.println(a);
+        Son son = new Son();
+        son.setName("s1");
+        Son son2 = new Son();
+        son2.setName("s2");
+        ArrayList<Son> sons = new ArrayList<>();
+        sons.add(son);
+        sons.add(son2);
+        Map<String, List<? extends Parent>> aa = Collections.singletonMap("aa", sons);
+        test1(aa);
+
+    }
+
+    public static void test1(Map<String, List<? extends Parent>> person){
+
+//        person.forEach(e -> System.out.println(e.getName()));
     }
 }
