185cb849189268177d8bab7c134ab3d1e0ce4231
feiyi <feiyi@zenki.ai>
2021-03-19 18:41:25 +0800

    redis

--- src/test/java/com/widget/db/redis/RedisTest.java
+++ src/test/java/com/widget/db/redis/RedisTest.java
@@ -6,10 +6,12 @@ import redis.clients.jedis.Jedis;
  *  Created by shao.guangze on 2018/7/4
  */
 public class RedisTest {
+
     public static void main(String[] args) {
         Jedis j = new Jedis("127.0.0.1", 6379);
-        //NX 不存在时才set；XX 存在时才set；EX 秒；PX毫秒
-        j.set("jedisDemo", "Sida", "XX", "EX", 3);
+        //NX 不存在时才set；XX 存在时才set；
+        //EX 秒；PX毫秒
+        j.set("jedisDemo", "Sida", "NX", "EX", 3);
         try {
             Thread.sleep(2000L);
         } catch (InterruptedException e) {
@@ -24,4 +26,5 @@ public class RedisTest {
         System.out.println(j.get("jedisDemo"));//null
         j.close();
     }
+
 }
---
+++ src/test/java/com/widget/db/redis/RedisTest4.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C), 2018-2021, zenki.ai
+ * FileName: RedisTest4
+ * Author:   feiyi
+ * Date:     2021/3/19 6:32 PM
+ * Description: RedisTest4
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.widget.db.redis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * 〈一句话功能简述〉:
+ * 〈RedisTest4
+ *   set, 无序且不重复的集合。
+ *  〉
+ *
+ * @author feiyi
+ * @create 2021/3/19
+ * @since 1.0.0
+ */
+public class RedisTest4 {
+
+    private static final String set_key = "set-key";
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        jedis.sadd(set_key, "v-1");
+        jedis.sadd(set_key, "v-2");
+        jedis.sadd(set_key, "v-1");
+        System.out.println(jedis.smembers(set_key));
+    }
+
+}
